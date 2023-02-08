befcde23ab95a8807484a76b79550ef4660129ca
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-08 17:59:46 +0530

    integrated login and logout functionalities

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,8 +13,27 @@ const register = async (userData) => {
 	return response.data;
 };
 
+// Login User
+const login = async (userData) => {
+	const response = await axios.post(API_URL + 'login', userData);
+
+	if (response.data) {
+		localStorage.setItem('user', JSON.stringify(response.data));
+	}
+
+	return response.data;
+};
+
+//logout
+
+const logout = async () => {
+	localStorage.removeItem('user');
+};
+
 const authService = {
 	register,
+	logout,
+	login,
 };
 
 export default authService;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,12 @@
 import { useState,useEffect } from "react"
 import { FaSignInAlt } from "react-icons/fa"
+import {useSelector,useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login,reset} from '../features/auth/authSlice'
+
+
+import Spinner from "../components/Spinner"
 
 
 
@@ -12,6 +19,29 @@ function Login() {
 
     const {email,password} =formData
 
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+
+    const {user,isLoading,isError,isSuccess,message}=useSelector((state)=>state.auth)
+
+    useEffect(()=>{
+        if(isError){
+            toast.error(message)
+        }
+        if(isSuccess || user){
+            navigate('/')
+        
+        }
+        
+        
+        dispatch(reset())
+        
+        
+        },[user,isError,isSuccess,message,navigate,dispatch])
+
+
+
 const onChange = (e) => {
     setFormData((prevState)=>({
         ...prevState,
@@ -22,7 +52,23 @@ const onChange = (e) => {
 
 const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData ={
+        email,
+        password,
+
+    }
+
+    dispatch(login(userData))
 }
+
+
+if(isLoading)
+{
+    return <Spinner/>
+}
+
+
   return (
     <>
     <section className="heading">
