1b3072589cd33d6f616f62f360c57ea2ca41f689
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-09 16:49:57 +0530

    ready for deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/healthcheck', (req, res) => {
-	res.status(200).json({
-		status: 'healthy',
-	});
-});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+		)
+	);
+} else {
+	app.get('/', (req, res) => res.send('please set to production...'));
+}
 
 app.use('/api/goals', goalRoutes);
 app.use('/api/users', userRoutes);
