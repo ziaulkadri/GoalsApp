9fc3778df65df7410ddaa42a9b5f082751f91996
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-09 14:42:41 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const color = require('colors');
 const dotenv = require('dotenv').config();
-
+const path = require('path');
 const goalRoutes = require('./routes/goalsRoutes');
 const userRoutes = require('./routes/userRoutes');
 
@@ -19,6 +19,20 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// serve frontend
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+		)
+	);
+} else {
+	app.get('/', (req, res) => res.send('please set to production...'));
+}
 
 app.use('/api/goals', goalRoutes);
 app.use('/api/users', userRoutes);
